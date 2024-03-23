04b06da6df44f0478c0ba5b80e303bfe4653a9f9
sudeesh kumar <sudeeshkumar@sudeeshs-MacBook-Air.local>
2024-03-23 12:31:40 +0530

    menu layout fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
       <section
         className={`w-[30%] max-[850px]:absolute top-0 left-0 flex-shrink-0 max-[850px]:w-full px-3 max-[850px]:mx-0 max-[650px]:my-0 duration-500 ease-in-out
       max-[650px]:px-5 max-[850px]:py-5 max-[850px]:border-none mx-3 rounded-xl my-4 py-4 border
-      max-[1050px]:py-8 max-[1050px]:px-4 max-w-[450px]
+      max-[1050px]:py-8 max-[1050px]:px-4 
       max-[1050px]:w-[30%] overflow-hidden ${
         id === "product" ? "max-[850px]:translate-x-[-100%]" : ""
       }`}
